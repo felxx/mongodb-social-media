5a9c2aca48115a2cc44a19e088bf9764f9384c48
felxx <igorfelipemonteirocontato@gmail.com>
2025-06-20 15:22:28 -0300

    project created

---
+++ src/main/java/com/felxx/mongodb_social_media/MongodbSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.felxx.mongodb_social_media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbSocialMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felxx/mongodb_social_media/MongodbSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felxx.mongodb_social_media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbSocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
