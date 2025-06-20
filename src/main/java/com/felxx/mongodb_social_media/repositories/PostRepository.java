1608cc0f884d6f3267885b35f59c80c6ea416b93
felxx <igorfelipemonteirocontato@gmail.com>
2025-06-20 16:58:43 -0300

    feat: implement post entity and repository

---
+++ src/main/java/com/felxx/mongodb_social_media/repositories/PostRepository.java
@@ -0,0 +1,10 @@
+package com.felxx.mongodb_social_media.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felxx.mongodb_social_media.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
