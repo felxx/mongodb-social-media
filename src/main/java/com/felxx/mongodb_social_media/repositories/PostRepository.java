10b602fbf1e6571652a934575f27a15e13933de7
felxx <igorfelipemonteirocontato@gmail.com>
2025-06-23 13:56:48 -0300

    feat: implement full search functionality for posts with date range

--- src/main/java/com/felxx/mongodb_social_media/repositories/PostRepository.java
+++ src/main/java/com/felxx/mongodb_social_media/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.felxx.mongodb_social_media.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -10,9 +11,12 @@ import com.felxx.mongodb_social_media.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-    
+
     @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
     List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'date': { $gte: ?1, $lte: ?2 } } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/felxx/mongodb_social_media/services/PostService.java
+++ src/main/java/com/felxx/mongodb_social_media/services/PostService.java
@@ -1,5 +1,6 @@
 package com.felxx.mongodb_social_media.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
         return postRepository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
     }
 }
