79d344a83de32fdd7be8d393d62f4f78cf3b520b
felxx <igorfelipemonteirocontato@gmail.com>
2025-06-23 13:43:13 -0300

    feat: implement regex search for titles

--- src/main/java/com/felxx/mongodb_social_media/repositories/PostRepository.java
+++ src/main/java/com/felxx/mongodb_social_media/repositories/PostRepository.java
@@ -3,12 +3,16 @@ package com.felxx.mongodb_social_media.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.felxx.mongodb_social_media.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
